e5f798ecb62de5d464628f4c6fbcb0e1b111a05f
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2025-07-07 23:52:21 +0530

    Removed comments and reading p from environment variable

--- crypto.cpp
+++ crypto.cpp
@@ -3,20 +3,21 @@
 
 int main() {
 
+    const char* pENV = "pVariable";
 
-    // Creating big prime p variable
+    const char* pValue = getenv(pENV);
+
     mp_int p;
     mp_err mp_initialize = mp_init(&p);
     if(mp_initialize != MP_OKAY){
         printf("Error initializing mp: %s\n", mp_error_to_string(mp_initialize));
     }
-    mp_err error = mp_read_radix(&p, "FFFFFFFFFFFFFFFFADF85458A2BB4A9AAFDC5620273D3CF1D8B9C583CE2D3695A9E13641146433FBCC939DCE249B3EF97D2FE363630C75D8F681B202AEC4617AD3DF1ED5D5FD65612433F51F5F066ED0856365553DED1AF3B557135E7F57C935984F0C70E0E68B77E2A689DAF3EFE8721DF158A136ADE73530ACCA4F483A797ABC0AB182B324FB61D108A94BB2C8E3FBB96ADAB760D7F4681D4F42A3DE394DF4AE56EDE76372BB190B07A7C8EE0A6D709E02FCE1CDF7E2ECC03404CD28342F619172FE9CE98583FF8E4F1232EEF28183C3FE3B1B4C6FAD733BB5FCBC2EC22005C58EF1837D1683B2C6F34A26C1B2EFFA886B423861285C97FFFFFFFFFFFFFFFF", 16);
+    mp_err error = mp_read_radix(&p, pValue, 16);
     if(error != MP_OKAY){
         printf("Error reading radix: %s\n", mp_error_to_string(error));
     }
 
 
-    //Creating g = 2
     mp_int g;
     mp_err g_initialize = mp_init_i32(&g, 2);
     if(g_initialize != MP_OKAY){
@@ -24,7 +25,6 @@ int main() {
     }
 
 
-    // Creating private key A
     mp_int private_key_a;
     mp_err private_key_initialize_a = mp_init(&private_key_a);
     if(private_key_initialize_a != MP_OKAY){
@@ -36,7 +36,6 @@ int main() {
     }
 
 
-    // Creating private key B
     mp_int private_key_b;
     mp_err private_key_initialize_b = mp_init(&private_key_b);
     if(private_key_initialize_b != MP_OKAY){
@@ -48,7 +47,6 @@ int main() {
     }
 
 
-    //Creating public key A
     mp_int public_key_a;
     mp_err public_key_initialize_a = mp_init(&public_key_a);
     if(public_key_initialize_a != MP_OKAY){
@@ -60,7 +58,6 @@ int main() {
     }
 
 
-    //Creating public key B
     mp_int public_key_b;
     mp_err public_key_initialize_b = mp_init(&public_key_b);
     if(public_key_initialize_b != MP_OKAY){
@@ -72,7 +69,6 @@ int main() {
     }
 
     
-    //Creating shared key A
     mp_int shared_key_a;
     mp_err shared_key_initialize_a = mp_init(&shared_key_a);
     if(shared_key_initialize_a != MP_OKAY){
