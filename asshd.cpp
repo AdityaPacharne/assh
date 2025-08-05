7d0344b7077b91130e0cb78ebf3f0eca67e58e02
Aditya Pacharne <48561911+AdityaPacharne@users.noreply.github.com>
2025-08-05 14:22:23 +0530

    Fix type warnings in exec() and sha256()

--- asshd.cpp
+++ asshd.cpp
@@ -42,7 +42,8 @@ std::string exec(const char* cmd) {
      */
     std::array<char, 128> buffer;
     std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    using file_closer = int (*)(FILE*);
+    std::unique_ptr<FILE, file_closer> pipe(popen(cmd, "r"), pclose);
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
